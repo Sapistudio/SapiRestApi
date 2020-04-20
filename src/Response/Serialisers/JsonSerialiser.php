522268b35ce6e1d7dd7114d4659ec97b8bf4185c
laur sandu <Sapistudio@users.noreply.github.com>
2020-04-20 15:24:00 +0300

    Add files via upload

---
+++ src/Response/Serialisers/JsonSerialiser.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace SapiStudio\RestApi\Response\Serialisers;
+
+use SapiStudio\RestApi\Interfaces\Request\Serialiser;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer as SymfonySerialiser;
+
+/**
+ * Class JsonSerialiser.
+ */
+class JsonSerialiser implements Serialiser
+{
+    /**
+     * @param $input
+     *
+     * @return mixed
+     */
+    public function serialise($input)
+    {
+        return (new SymfonySerialiser(
+            [new ObjectNormalizer()], [new JsonEncoder()]
+        ))->serialize($input, 'json');
+    }
+}
