a0ccbc910e211c676f0f30e47397ac1fcb88f14b
laur sandu <Sapistudio@users.noreply.github.com>
2020-05-30 00:32:32 +0300

    Update InvalidResponseException.php

--- src/Exceptions/InvalidResponseException.php
+++ src/Exceptions/InvalidResponseException.php
@@ -1,32 +1,21 @@
 <?php
-
 namespace SapiStudio\RestApi\Exceptions;
 
 use Exception;
 
-/**
- * Class InvalidResponseException.
- */
+/** Class InvalidResponseException.*/
 class InvalidResponseException extends Exception
 {
-    /**
-     * @var array
-     */
     private $response;
 
-    /**
-     * InvalidResponseException constructor.
-     *
-     */
+    /** InvalidResponseException::__construct()*/
     public function __construct($message, $code = 0, Exception $previous = null, $response = [])
     {
         parent::__construct($message, $code, $previous);
         $this->response = $response;
     }
 
-    /**
-     * @return array
-     */
+    /** InvalidResponseException::getResponse() */
     public function getResponse()
     {
         return $this->response;
