70bdda9f754caf3d5617fdc10a14b06719a89f3e
laur sandu <Sapistudio@users.noreply.github.com>
2020-05-30 00:23:22 +0300

    Update AbstractApi.php

--- src/AbstractApi.php
+++ src/AbstractApi.php
@@ -3,28 +3,18 @@ namespace SapiStudio\RestApi;
 
 use SapiStudio\RestApi\Interfaces\HttpClient as HttpInterface;
 
-/**
- * Class AbstractApi.
- */
+/** Class AbstractApi.*/
 abstract class AbstractApi
 {
     protected $client;
 
-    /**
-     * AbstractApi constructor.
-     *
-     * @param HttpClient $client
-     */
+    /** AbstractApi constructor. */
     public function __construct(HttpInterface $client)
     {
         $this->client = $client;
     }
     
-    /**
-     * AbstractApi::__call()
-     * 
-     * @return
-     */
+    /** AbstractApi::__call()*/
     public function __call($name, $arguments)
     {
         return call_user_func_array([$this->client, $name], $arguments);
