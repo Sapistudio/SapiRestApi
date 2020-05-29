1237025633a4d18e1c61cfb64155f3621e49c2c9
laur sandu <Sapistudio@users.noreply.github.com>
2020-05-30 00:26:31 +0300

    Update ErrorHandler.php

--- src/Response/ErrorHandler.php
+++ src/Response/ErrorHandler.php
@@ -1,24 +1,15 @@
 <?php
-
 namespace SapiStudio\RestApi\Response;
 
 use SapiStudio\RestApi\Interfaces\Response\ErrorHandler as ResponseErrorHandler;
 use SapiStudio\RestApi\Exceptions\InvalidResponseException;
 
-/**
- * Class ErrorHandler.
- */
+/** Class ErrorHandler.*/
 class ErrorHandler implements ResponseErrorHandler
 {
     public function handle(array $data)
     {
-        if (empty($data)) {
-            throw new InvalidResponseException(
-                'Empty response received',
-                400,
-                null,
-                $data
-            );
-        }
+        if (empty($data))
+            throw new InvalidResponseException('Empty response received',400,null,$data);
     }
 }
